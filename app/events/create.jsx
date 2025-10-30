4644c2b1304f1eed0b5282ac8c3b7c69801c1222
INIEMEM <71082545+INIEMEM@users.noreply.github.com>
2025-10-30 23:38:33 +0100

    testinf update

--- app/events/create.jsx
+++ app/events/create.jsx
@@ -777,7 +777,7 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: 14,
-    color: '#444',
+    color: '#333',
     marginBottom: 8,
     fontFamily: 'Poppins_500Medium',
     fontWeight: '500',
